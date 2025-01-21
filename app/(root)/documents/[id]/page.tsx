d322586d12b1ee7a45100e562430ea55cc7ff92a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-21 18:48:37 +0100

    Refactor Document component to integrate CollaborativeRoom and enhance layout structure

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,14 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/ui/Header";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import CollabrativeRoom from "@/components/ui/CollabrativeRoom";
 
 const Document = () => {
   return (
-    <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This is for testing....Happy Coding</p>
-        </div>
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor />
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollabrativeRoom 
+      />
+    </main>
   );
 };
 
