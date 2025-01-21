15565d61734009fe63e0eec01e52d3c8a2b6cf56
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-01-21 19:22:45 +0100

    Add user resolution functions and integrate ActiveCollaborators in CollaborativeRoom

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Loader from "@/components/ui/Loader";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/User.action";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -9,11 +10,26 @@ import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
 
 export default Provider;
- 
--- components/ui/CollabrativeRoom.tsx
+++ components/ui/CollabrativeRoom.tsx
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/ui/Header";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import ActiveCollaborators from "./ActiveCollaborators";
 
 const CollabrativeRoom = () => {
   return (
@@ -17,12 +18,15 @@ const CollabrativeRoom = () => {
                 This is for testing....Happy Coding
               </p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
